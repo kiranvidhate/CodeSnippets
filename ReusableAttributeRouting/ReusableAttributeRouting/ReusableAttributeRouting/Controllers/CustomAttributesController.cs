dbafc8656e49efdfef77aa2c40edddee1bb3e553
Kiran Vidhate <kiran1781w@gmail.com>
2017-05-18 16:07:50 +0530

    MVC Attribute Rounding reusable snippet added

---
+++ ReusableAttributeRouting/ReusableAttributeRouting/ReusableAttributeRouting/Controllers/CustomAttributesController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+
+namespace ReusableAttributeRouting.Controllers
+{
+    [RoutePrefix("MyRoute")]
+    public class CustomAttributesController : Controller
+    {
+        [Route("~/")] //Specifies that this is the default action for the entire application. Route: /
+
+        [Route("")] //Specifies that this is the default action for this route prefix. Route: /Users
+
+        //Simple Attribute Routing
+        [Route("Simple")]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is Simple Attribute Routing Example.";
+            return View("Index");
+        }
+
+        //Single Parameter Attribute Routing
+        [Route("WithId/{id}")]
+        public ActionResult ViewWithParameter(int id)
+        {
+            ViewBag.Message = "This is Attribute Routing with ID:" + id;
+            return View("Index");
+        }
+
+        //Multiple Parameter Attribute Routing
+        //We can also specify if a parameter is optional by using ?:
+        [Route("WithMulti/{id}/{name?}")]
+        public ActionResult ViewWithMultipleParameter(int id, string name)
+        {
+            ViewBag.Message = "This is Attribute Routing with ID:" + id + " and Name:" + name;
+            return View("Index");
+        }
+
+        //Multiple Parameter Attribute Routing
+        //We can also specify if a parameter is optional by using ?:
+        [Route("WithIntId/{id:int}")]
+        public ActionResult ViewWithDataTypeParameter(int id)
+        {
+            ViewBag.Message = "This is Attribute Routing with Integer ID:" + id;
+            return View("Index");
+        }
+    }
+}
---
+++ ReusableAttributeRouting/ReusableAttributeRouting/ReusableAttributeRouting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReusableAttributeRouting.Startup))]
+namespace ReusableAttributeRouting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
