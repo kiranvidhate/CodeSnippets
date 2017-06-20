df5e544360e679cadd75fb09a49432c9f6044ef1
Kiran Vidhate <kiran1781w@gmail.com>
2017-06-20 15:54:11 +0530

    Localization files added

---
+++ Reusable-LocalizationMVC/localizationNGlobalizationSample/localizationNGlobalizationSample/Global.asax.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Threading;
+
+namespace localizationNGlobalizationSample
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void Application_BeginRequest(Object source, EventArgs e)
+        {
+            HttpApplication application = (HttpApplication)source;
+            HttpContext context = application.Context;
+
+            string culture = null;
+
+
+            if (context.Request.UserLanguages != null && Request.UserLanguages.Length > 0)
+            {
+                //http://afana.me/post/aspnet-mvc-internationalization.aspx
+                //culture = Request.UserLanguages[0].Split(';')[0];
+                culture = "ar";// "hi";// "en-US";
+                Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(culture);
+                Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+            }
+        }
+
+    }
+}
