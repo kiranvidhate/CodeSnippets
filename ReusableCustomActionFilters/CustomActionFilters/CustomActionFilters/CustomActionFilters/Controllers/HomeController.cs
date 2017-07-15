d46f04e764e4478d3f4b36d59c7d34f92c66dc5a
Kiran Vidhate <kiran1781w@gmail.com>
2017-07-15 14:08:43 +0530

    Custom action files added

---
+++ ReusableCustomActionFilters/CustomActionFilters/CustomActionFilters/CustomActionFilters/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using CustomActionFilters.Common;
+
+namespace CustomActionFilters.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Indexes this instance.
+        /// </summary>
+        /// <returns></returns>
+        [TrackExecutionTime]
+        public ViewResult Index()
+        {
+            ViewBag.results = "Index Action Invoked";
+            return View();
+        }
+
+        /// <summary>
+        /// Welcomes this instance.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.Exception">Exception ocuured</exception>
+        [TrackExecutionTime]
+        public string Welcome()
+        {
+            throw new Exception("Exception ocuured");
+        }
+    }
+}
